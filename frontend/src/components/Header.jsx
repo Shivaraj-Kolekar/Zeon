4b943e92e29424d4741c55c54e7ecf66efe052ef
Shivraj <shivkolekar01@gmail.com>
2024-09-18 12:46:25 +0530

    added header and vercel file changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-day-picker'
 import { Link } from 'react-router-dom'
 import { ModeToggle } from './mode-toggle'
-import { UserIcon } from '@heroicons/react/24/outline'
 import UserActions from './UserActions'
+
 function Header () {
-  const [isLogin, setLogin] = useState(null)
-  const user = localStorage.getItem('token')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem('token')
+      setIsLoggedIn(!!token)
+    }
+
+    checkLoginStatus()
+    window.addEventListener('storage', checkLoginStatus)
+
+    return () => {
+      window.removeEventListener('storage', checkLoginStatus)
+    }
+  }, [])
+
   return (
-    <header className='md:py-5 p-3 md:px-8 text-xl bg-slate-300  border-b-slate-500 dark:border-b-slate-700 border dark:bg-slate-900'>
+    <header className='md:py-5 p-3 md:px-8 text-xl bg-slate-300 border-b-slate-500 dark:border-b-slate-700 border dark:bg-slate-900'>
       <nav className='flex justify-between items-center'>
         <div>
           <Link to='/'>
             <h1 id='logo'>ZEON</h1>
           </Link>
         </div>
-        {!user ? (
+        {!isLoggedIn ? (
           <ul className='flex flex-row gap-2 items-center md:gap-5'>
             <li>
               <Link to='/login'>
@@ -23,7 +37,7 @@ function Header () {
               </Link>
             </li>
             <li>
-              <ModeToggle></ModeToggle>
+              <ModeToggle />
             </li>
           </ul>
         ) : (
@@ -38,10 +52,10 @@ function Header () {
               <Link to='/dashboard/assets'>Assets</Link>
             </li>
             <li>
-              <UserActions></UserActions>
+              <UserActions />
             </li>
             <li>
-              <ModeToggle></ModeToggle>
+              <ModeToggle />
             </li>
           </ul>
         )}
